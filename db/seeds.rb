62547c862fe6a9482057b91a0eff6f51f74262df
Tom Brennan <tommybrennan@gmail.com.com>
2013-10-19 06:41:02 -0400

    I seeded my database and ran rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Question.destroy_all
+
+Question.create(category: 'sports', answer: 'LONG JUMP', difficulty: '1', question: 'Jesse Owen''s world record in which sporting event stood from 1935 until 1960?')
+Question.create(category: 'sports', answer: 'CHICAGO WHITE SOX', difficulty: '1', question: 'When Michael Jordan retired from the Chicago Bulls for the first time in 1994, he signed a minor-league contract with what major league baseball team?')
+Question.create(category: 'sports', answer: 'BUSTER DOUGLAS', difficulty: '1', question: 'Is Mike Tyson unbeatable? On February 10, 1990, in the heavyweight championship fight in Tokyo, this boxer knocked out Mike Tyson in the tenth round, the only fighter ever to defeat Tyson for the heavyweight boxing crown. Who was he?')
+Question.create(category: 'sports', answer: 'STEEPLECHASE', difficulty: '1', question: 'This Olympic event is a 3,000 meter footrace around a track.  Runners must jump four hurdles and a water obstacle.  What is this event called?')
+Question.create(category: 'sports', answer: 'Joe Louis', difficulty: '1', question: 'Under what name did Joseph L. Barrow gain fame in the world of sports?')
+Question.create(category: 'animal', answer: 'ERMINE', difficulty: '1', question: 'By what name do we identify those certain kinds of weasels whose coats change color in the winter?')
+Question.create(category: 'animal', answer: 'cheetah', difficulty: '1', question: 'what big cat cannot retract its claws?')
+Question.create(category: 'animal', answer: '3 months', difficulty: '1', question: 'how long can a whale go without eating?')
+Question.create(category: 'animal', answer: 'Hands', difficulty: '1', question: 'What unit of measurement is used to measure a horse''s height in?')
+Question.create(category: 'animal', answer: 'Blue Whale', difficulty: '1', question: 'The largest mammal that ever lived weighs more than 100 tons. What kind of animal is this?')
+
+
+
+
+
+
+
+
+
+
+
+
+
