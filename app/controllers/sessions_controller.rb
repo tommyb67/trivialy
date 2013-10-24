c92e3227ec67e819046bab3fd5bde8130b31b87d
Tom Brennan <tommybrennan@gmail.com.com>
2013-10-24 10:31:14 -0400

    log in on create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      log_in(user)
       redirect_to user_path(user)
     else
       redirect_to new_session_path
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,4 +12,8 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 end
